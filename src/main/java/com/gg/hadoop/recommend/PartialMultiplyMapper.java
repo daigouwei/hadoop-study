c342fe7a3670e4939aa3215fa4491b433932caae
daigouwei <daigouwei@gmail.com>
2018-09-17 17:48:22 +0800

    将共现矩阵和用户向量合并

---
+++ src/main/java/com/gg/hadoop/recommend/PartialMultiplyMapper.java
@@ -0,0 +1,32 @@
+package com.gg.hadoop.recommend;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.mahout.cf.taste.hadoop.item.VectorAndPrefsWritable;
+import org.apache.mahout.math.VarLongWritable;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.VectorWritable;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author guowei
+ * @date 2018/9/16
+ */
+public class PartialMultiplyMapper
+        extends Mapper<IntWritable, VectorAndPrefsWritable, VarLongWritable, VectorWritable> {
+    @Override
+    public void map(IntWritable key, VectorAndPrefsWritable vectorAndPrefsWritable, Context context)
+            throws IOException, InterruptedException {
+        Vector cooccurrenceColumn = vectorAndPrefsWritable.getVector();
+        List<Long> userIDs = vectorAndPrefsWritable.getUserIDs();
+        List<Float> prefValues = vectorAndPrefsWritable.getValues();
+        for (int i = 0; i < userIDs.size(); i++) {
+            long userID = userIDs.get(i);
+            float prefValue = prefValues.get(i);
+            Vector partialProduct = cooccurrenceColumn.times(prefValue);
+            context.write(new VarLongWritable(userID), new VectorWritable(partialProduct));
+        }
+    }
+}
